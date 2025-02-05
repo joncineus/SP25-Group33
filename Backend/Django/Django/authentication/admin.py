8182ec05d2b84ce37dbe8a81e6918cc90654d027
Felipe <faf19a@fsu.edu>
2025-02-05 16:53:57 -0500

    setting up users, password hashing

--- Backend/Django/Django/authentication/admin.py
+++ Backend/Django/Django/authentication/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# Extend the default UserAdmin to include the custom fields like `role`
+class CustomUserAdmin(UserAdmin):
+    # Fields to display in the admin list view
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    
+    # Adding `role` to the user form inside the admin panel
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+# Register the CustomUser model
+admin.site.register(CustomUser, CustomUserAdmin)
