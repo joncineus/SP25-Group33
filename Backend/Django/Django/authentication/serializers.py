950b43b347449f9404cfa2a4b53e5d5043117b87
manarmohsin <manarmohsin9@gmail.com>
2025-02-06 22:59:41 -0500

    Finished issue-1.3(Backend). Immplemented user registeration API. Added serializers and urls for authentication.

---
+++ Backend/Django/Django/authentication/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from .models import CustomUser  
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
