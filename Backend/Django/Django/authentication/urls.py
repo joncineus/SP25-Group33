c415310e84d6976d09367d1e5f90f8abec15b35e
Jonathan Alcineus <95049403+joncineus@users.noreply.github.com>
2025-02-28 12:34:55 -0500

    Actual changes to the urls page

--- Backend/Django/Django/authentication/urls.py
+++ Backend/Django/Django/authentication/urls.py
@@ -6,10 +6,10 @@ from .views import QuizUpdateView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('quizzes/create/', QuizCreateView.as_view(), name='quiz-create'),
-    path('quizzes/<int:pk>/', QuizUpdateView.as_view(), name='quiz-update'), 
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/quizzes/create/', QuizCreateView.as_view(), name='quiz-create'),
+    path('auth/quizzes/<int:pk>/', QuizUpdateView.as_view(), name='quiz-update'), 
 
 ]
 
